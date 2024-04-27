9475140a1e39c190cc80c67fdc04ebc1229bd55d
joshwilson1212 <joshuawilson2001@gmail.com>
2024-04-27 02:30:40 -0500

    set my scene, wrapped up prefabs, imported sprites

--- Assets/PersonalFolders/Josh/shopManagerScript.cs
+++ Assets/PersonalFolders/Josh/shopManagerScript.cs
@@ -6,11 +6,13 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class shopManagerScript : MonoBehaviour{
 
+    //initializes the shop item array
     public int[,] shopItem = new int[5, 5];
     public Player player;
     public TextMeshProUGUI coinUI;
 
     void Start(){
+        //gets the info of the coin counter an sets its initial value
         coinUI.GetComponent<TextMeshProUGUI>();
         coinUI.text = "Coins:"+player.coinCounter.ToString();
         
@@ -30,6 +32,7 @@ public class shopManagerScript : MonoBehaviour{
         coinUI.text = "Coins:" + player.coinCounter.ToString();
     }
 
+    //will ask the even system for the information on the button you just clicked and will find the information attached to it and reduce the coins you have for the ammount it cost
     public void Buy(){
         GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
         if (player.coinCounter >= shopItem[2, ButtonRef.GetComponent<buttonInfo>().ItemID])
