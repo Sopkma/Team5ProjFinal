61ac40767bdd30c8ff3f053ff6df5c9a5b149a3d
joshwilson1212 <joshuawilson2001@gmail.com>
2024-05-07 01:29:54 -0500

    commented our compilation error

--- Assets/PersonalFolders/Jeremy/Scripts/HealthManager.cs
+++ Assets/PersonalFolders/Jeremy/Scripts/HealthManager.cs
@@ -32,7 +32,7 @@ public class HealthManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        groundLayer = LayerMask.GetMask("Ground");
+        //groundLayer = LayerMask.GetMask("Ground");
         scoreManager = FindAnyObjectByType<ScoreManager>();
     }
 
